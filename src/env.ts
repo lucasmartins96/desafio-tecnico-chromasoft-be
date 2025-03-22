c7f784d53d94105f935e9912aa148e57cde0f990
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2025-03-22 12:04:10 -0300

    chore: mapear variáveis de ambiente

---
+++ src/env.ts
@@ -0,0 +1,13 @@
+const environmentVar = {
+	port: Number(process.env.PORT) || 3000,
+	secretKey: process.env.SECRET_KEY_JWT,
+	db: {
+		user: process.env.DB_USER,
+		host: process.env.DB_HOST,
+		port: Number(process.env.DB_PORT),
+		password: process.env.DB_PASSWORD,
+		name: process.env.DB_NAME,
+	},
+};
+
+export default environmentVar;
